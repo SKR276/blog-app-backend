0ee494aabb78b0713468648e28ebfe41d75b24a4
SKR276 <sreeponnu06@gmail.com>
2024-02-09 11:29:22 +0530

    add security

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -2,14 +2,30 @@ const express=require("express")
 const userModel=require("../models/userModel")
 
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
 router.post("/add",async(req,res)=>{
-    let data=req.body
-    let userObj=new userModel(data)
-    let result=await userObj.save()
+    let {data}={"data":req.body}
+    let password=data.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.password=hashedPassword
+            console.log(data)
+            let userObj=new userModel(data)
+
+    let result= userObj.save()
     res.json({
         status:"success"
     })
+        }
+    )
+
 })
 
 module.exports=router
